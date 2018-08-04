43e50aefa70e60c25faf37f7d84767ce8e34f007
Hilarion18 <hilarion_cross@hotmail.com>
2018-08-05 04:38:17 +0700

    week-2

---
+++ exercise7.js
@@ -0,0 +1,89 @@
+/*
+1. Menyusun Barisan Bintang
+
+Problem
+
+Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript untuk menampilkan di console barisan asterisks (bintang). Setiap baris memiliki 1 simbol '*'.
+
+Skeleton Code
+
+var rows1; // input the number of rows
+
+// do loops to display asterisks in the console.
+Output
+
+jika rows1 = 5
+
+*
+*
+*
+*
+*
+*/
+/*
+function angka(input){
+    var deret;
+    for (var y = 0; y < input; y++){
+        deret = '*'
+        console.log(deret)
+        }
+    }
+angka(5)
+*/
+
+/*
+2. Menyusun Barisan Bintang Dengan Nested Looping
+
+Problem
+
+Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript untuk menampilkan di console barisan asterisks (bintang). Setiap baris memiliki jumlah simbol '*' sesuai dengan jumlah baris. Manfaatkan nested loop atau loop di dalam loop untuk menyelesaikan kasus ini.
+
+Skeleton Code
+
+var rows2; // input the number of rows
+
+// do loops to display asterisks in the console.
+Output
+
+jika rows2 = 5
+
+*****
+*****
+*****
+*****
+*****
+*/
+
+function angka(input){
+    var deret;
+    for (var a = 0; a < input; a++){
+        deret = '*'
+        deret = deret+deret+deret+deret+deret
+        console.log(deret)
+        }
+    } 
+angka(5)
+
+
+/*
+3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
+
+Problem
+
+Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript untuk menampilkan di console barisan asterisks (bintang) dalam bentuk tangga. Setiap baris memiliki jumlah simbol '*' sesuai dengan nomor baris. Baris pertama, hanya ada satu bintang. Baris kedua, dua bintang, baris ketiga tiga bintang, dan seterusnya. Manfaatkan nested loop atau loop di dalam loop untuk menyelesaikan kasus ini.
+
+Skeleton Code
+
+var rows3; // input the number of rows
+
+// do loops to display asterisks in the console.
+Output
+
+jika rows3 = 5
+
+*
+**
+***
+****
+*****
+*/
